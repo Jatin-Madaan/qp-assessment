2a69b81810b9f16f0627b9cc66fffb20ba8b5eea
Jatin Madaan <jatinmadaan9882@gmail.com>
2024-04-14 15:23:42 +0530

    added annotation

--- src/main/java/com/questionpro/grocery/repository/GroceryRepository.java
+++ src/main/java/com/questionpro/grocery/repository/GroceryRepository.java
@@ -3,6 +3,8 @@ package com.questionpro.grocery.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.questionpro.grocery.entity.Grocery;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GroceryRepository extends JpaRepository<Grocery, Long> {
 }
